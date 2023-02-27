25de46e51608082d1f0e354ce176571e65b9eab3
vietanhp <vietanhpham1111@gmail.com>
2023-02-27 19:39:43 +0700

    add admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-	belongs_to :category
-	has_many :feedback, dependent: :destroy
-	has_many :order_lists
-	has_one_attached :image
+  belongs_to :category
+  has_many :feedback, dependent: :destroy
+  has_many :order_lists
+  has_one_attached :image
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+module.exports = environment
+// Add the following lines
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
+}))
+// End new addition
 module.exports = environment
